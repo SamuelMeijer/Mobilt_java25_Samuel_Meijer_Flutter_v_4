eee27e7a415708f066055bda8da6771813f93ffe
Samuel Meijer <samuel.j.meijer@gmail.com>
2026-09-17 13:45:19 +0200

    Adding functionality to save a username to sharedPref

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // Shared Pref. and controllers
   final SharedPreferencesAsync sharedPref = SharedPreferencesAsync();
+  final TextEditingController _usernameController = TextEditingController();
+  // States
   String? _username;
   bool _isLoading = true;
+  bool _isSaving = false;
 
   @override
   void initState() {
@@ -52,10 +56,32 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _saveUsername() async {
+    final enteredUsername = _usernameController.text.trim();
+
+    if (enteredUsername.isEmpty) {
+      // TODO: Add feedback to user?
+      return;
+    }
+
+    setState(() {
+      _isSaving = true;
+    });
+
+    await sharedPref.setString('useranme', enteredUsername);
+    // Preventing setState to be called on a page no longer in use, if the user changes viewing page while the async function is waiting for await result to finish
+    if (!mounted) return;
+
+    setState(() {
+      _username = enteredUsername;
+      _isSaving = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_isLoading) {
-      return const Scaffold(body: Center(child: Text("LOADING...")));
+      return Scaffold(body: Center(child: Text("LOADING...")));
     }
 
     return Scaffold(
@@ -71,6 +97,20 @@ class _MyHomePageState extends State<MyHomePage> {
               'Welcome ${_username ?? 'Guest'}!',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+
+            // TODO: Fix styling - Covers whole screen atm.
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Update username',
+                hintText: 'Enter your new username',
+              ),
+              controller: _usernameController,
+            ),
+            ElevatedButton(
+              onPressed: _isSaving ? null : _saveUsername,
+              child: Text('Save username'),
+            ),
+
             ElevatedButton(
               onPressed: () => {
                 Navigator.push(
